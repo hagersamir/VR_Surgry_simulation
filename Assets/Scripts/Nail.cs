5f4dd872e7abc81316ec0388753d024a555d0b31
Salma Ashraf <salmabenashrafbenfathy@gmail.com>
2025-06-05 14:48:01 +0300

    Adjust Nail Location

--- Assets/Scripts/Nail.cs
+++ Assets/Scripts/Nail.cs
@@ -7,8 +7,9 @@ public class Nail : MonoBehaviour
     public EventManager eventManager;  // Assign in inspector
 
     // Target snap position and rotation
-    private Vector3 snapPosition = new Vector3(-0.014f, 1.207f, -0.2775f);
-    private Quaternion snapRotation = Quaternion.Euler(58.928f, 182.693f, 2.088f);
+    private Vector3 snapPosition = new Vector3(-0.014f, 1.208f, -0.271f);
+    // private Quaternion snapRotation = Quaternion.Euler(58.928f, 182.693f, 2.088f);
+    private Quaternion snapRotation = Quaternion.Euler(57.6535378f,184.882233f,4.98095989f);
     private void OnTriggerEnter(Collider other)
     {
         // player has finished using the T-Handle Guide wire 
