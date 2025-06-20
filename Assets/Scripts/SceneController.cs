a6ccf5139cb88aed15405465c59007454354745f
hagersamir <105936147+hagersamir@users.noreply.github.com>
2025-06-20 19:34:31 +0300

    the bone is more broken

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -7,24 +7,14 @@ public class BoneSelectorUI : MonoBehaviour
   // public GameObject[] bonesToShow;
   public GameObject Bone1;
   public GameObject Bone2;
-  public GameObject Bone3;
+  // public GameObject Bone3;
 
   void Start()
   {
     popUpWindow.SetActive(true);
 
-    // // Hide all bones initially
-    // foreach (var bone in bonesToShow)
-    // {
-    //   bone.SetActive(false);
-    // }
   }
 
-  // public void OnCaseSelected(int caseIndex)
-  // {
-  //   popUpWindow.SetActive(false);
-
-  // }
 
   public void Case1()
   {
@@ -36,11 +26,11 @@ public class BoneSelectorUI : MonoBehaviour
     popUpWindow.SetActive(false);
     Bone2.SetActive(true);
   }
-  public void Case3()
-  {
-    popUpWindow.SetActive(false);
-    Bone3.SetActive(true);
-  }
+  // public void Case3()
+  // {
+  //   popUpWindow.SetActive(false);
+  //   Bone3.SetActive(true);
+  // }
 
   void Update()
   {
