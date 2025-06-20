058471acf0d20cacb4999868e610ac1c17032232
hagersamir <105936147+hagersamir@users.noreply.github.com>
2025-06-20 18:32:43 +0300

    required x rays

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,29 +1,51 @@
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 
 public class BoneSelectorUI : MonoBehaviour
 {
-    public GameObject popUpWindow;      
-    public GameObject[] bonesToShow;    
+  public GameObject popUpWindow;
+  // public GameObject[] bonesToShow;
+  public GameObject Bone1;
+  public GameObject Bone2;
+  public GameObject Bone3;
 
-    void Start()
-    {
-        popUpWindow.SetActive(true);
+  void Start()
+  {
+    popUpWindow.SetActive(true);
 
-        // Hide all bones initially
-        foreach (var bone in bonesToShow)
-        {
-            bone.SetActive(false);
-        }
+    // // Hide all bones initially
+    // foreach (var bone in bonesToShow)
+    // {
+    //   bone.SetActive(false);
+    // }
+  }
+
+  // public void OnCaseSelected(int caseIndex)
+  // {
+  //   popUpWindow.SetActive(false);
+
+  // }
+
+  public void Case1()
+  {
+    popUpWindow.SetActive(false);
+    Bone1.SetActive(true);
+  }
+  public void Case2()
+  {
+    popUpWindow.SetActive(false);
+    Bone2.SetActive(true);
+  }
+  public void Case3()
+  {
+    popUpWindow.SetActive(false);
+    Bone3.SetActive(true);
+  }
+
+  void Update()
+  {
+    if(Input.GetKeyDown(KeyCode.V)){
+      Case1();
     }
-
-    public void OnCaseSelected(int caseIndex)
-    {
-        popUpWindow.SetActive(false);
-
-        // Show only selected bone
-        for (int i = 0; i < bonesToShow.Length; i++)
-        {
-            bonesToShow[i].SetActive(i == caseIndex);
-        }
-    }
+  }
 }
